ee5988c414f44ce8ce1272985be6fd2eafcb7314
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-12-21 14:21:39 +0100

    loading screen

---
+++ erpmasterclasses/erpmasterclasses/src/app/[lang]/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from "framer-motion"
+import { ReloadIcon } from '@radix-ui/react-icons'
+
+
+function loading() {
+  return (
+    <div className='h-screen'></div>
+  )
+}
+
+export default loading
