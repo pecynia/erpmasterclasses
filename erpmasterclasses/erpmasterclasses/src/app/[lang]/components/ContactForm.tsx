9f88aec35ff6de95adf7adf843df5a38f6dfdd50
Nicolai Verheul <n.verheul@student.science.ru.nl>
2024-01-19 16:59:14 +0100

    Update ContactForm animation

--- erpmasterclasses/erpmasterclasses/src/app/[lang]/components/ContactForm.tsx
+++ erpmasterclasses/erpmasterclasses/src/app/[lang]/components/ContactForm.tsx
@@ -7,8 +7,8 @@ import { ClientContactFormProps } from './ClientContactForm'
 export default function ContactForm({ localization, errorMessages }: ClientContactFormProps) {
   return (
     <motion.div 
-          initial={{ opacity: 0, y: '100%' }}
-          whileInView={{ opacity: 1, y: '0%' }}
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ type: "spring", ease: "easeInOut", duration: 0.5 }}
           viewport={{ once: true }}
           className='min-w-[70%] lg:min-w-[40%] min-h-[20%] max-w-[80%] mb-20 pb-10 flex px-10 pt-4 rounded-xl  bg-white shadow-xl'
