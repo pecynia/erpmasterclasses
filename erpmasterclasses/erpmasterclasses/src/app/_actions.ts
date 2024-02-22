092ded6e6c1459c64453a93d3a71602f8358ce59
Nicolai Verheul <n.verheul@student.science.ru.nl>
2024-02-22 11:56:18 +0100

    Update recipient email address in sendCheckoutExpiredEmail function

--- erpmasterclasses/erpmasterclasses/src/app/_actions.ts
+++ erpmasterclasses/erpmasterclasses/src/app/_actions.ts
@@ -64,7 +64,7 @@ export async function sendCheckoutExpiredEmail(data: RegistrationFormProps) {
   try {
     const emailData = await resend.emails.send({
       from: 'ERP Masterclass <actions@erpmasterclasses.com>',
-      to: ['verheul.nicolai@gmail.com'], //['gk@erpmasterclasses.com'],
+      to: ['gk@erpmasterclasses.com'],
       subject: 'Checkout Expired',
       text: `Event: ${data.selectedEvent.title}\nCompany Name: ${data.companyName}\nAdress: ${data.address}\nCountry: ${data.country}\nName: ${data.nameParticipant}\nPhone: ${data.phone}\nEmail: ${data.email}\nPosition: ${data.position}\nVAT number: ${data.vatNumber}\nPO number: ${data.poNumber}\nAdditional participants: ${data.additionalParticipants}`,
       react: CheckoutExpiredEmail({ ...data }),
